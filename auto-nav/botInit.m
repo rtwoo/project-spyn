c74a15a041ed46f91033891e93c7f031a8cdd4b7
Ryan Woo <ryanw.contact@gmail.com>
2021-10-25 00:29:33 -0700

    rough draft autonomous nav loop and turn funcs

--- auto-nav/botInit.m
+++ auto-nav/botInit.m
@@ -1,31 +1,63 @@
 function botInit()
-	global inAuto wheelCircum turnDiam;
-	inAuto = true;
+	global IN_AUTO IN_DRIVE WHEEL_CIRCUM TURN_DIAM TURN_SPEED DRIVE_SPEED PORTS COLORS;
+	% set up color sensor to be RGB
+	IN_AUTO = true;
+	IN_DRIVE = false;
 	wheelDiam = 5.5;
-	wheelCircum = pi * wheelDiam;
+	WHEEL_CIRCUM = pi * wheelDiam;
+	% PORTS = map
+	% COLORS = map
 end
 
-function turn(degrees)
+function startDrive()
+	brick.MoveMotor(strcat(PORTS('RightMotor'), PORTS('LeftMotor')), DRIVE_SPEED);
+end
 
+function stopDrive()
+	brick.StopAllMotors('Brake');
+end
+
+function turn(degrees, direction)
+	global WHEEL_CIRCUM TURN_DIAM TURN_SPEED;
+	radian = TURN_DIAM / 2;
+	turnDist = deg2rad(degrees) * radian;
+	numRot = turnDist / WHEEL_CIRCUM;
+	dirSpeed = TURN_SPEED;
+	if diretion == 'LEFT'
+		dirSpeed = -dirSpeed;
+	end
+	brick.MoveMotorAngleRel(PORTS('RightMotor'), -TURN_SPEED, numRot * 360, 'Brake');
+	brick.MoveMotorAngleRel(PORTS('LeftMotor'), TURN_SPEED, numRot * 360, 'Brake');
 end
 
 function turnRight()
-	global wheelCircum, turnDiam;
-	radian = turnDiam / 2;
+	global WHEEL_CIRCUM TURN_DIAM TURN_SPEED;
+	radian = TURN_DIAM / 2;
 	turnDist = pi / 2 * radian;
-	numRot = turnDist / wheelCircum;
-	% power left for numRot rotations
-	% power right for -numRot rotations
+	numRot = turnDist / WHEEL_CIRCUM;
+	brick.MoveMotorAngleRel(PORTS('RightMotor'), -TURN_SPEED, numRot * 360, 'Brake');
+	brick.MoveMotorAngleRel(PORTS('LeftMotor'), TURN_SPEED, numRot * 360, 'Brake');
 end
 
 function turnLeft()
-
+	global WHEEL_CIRCUM TURN_DIAM TURN_SPEED;
+	radian = TURN_DIAM / 2;
+	turnDist = pi / 2 * radian;
+	numRot = turnDist / WHEEL_CIRCUM;
+	brick.MoveMotorAngleRel(PORTS('RightMotor'), TURN_SPEED, numRot * 360, 'Brake');
+	brick.MoveMotorAngleRel(PORTS('LeftMotor'), -TURN_SPEED, numRot * 360, 'Brake');
 end
 
 function turnAbout()
-
+	global WHEEL_CIRCUM TURN_DIAM TURN_SPEED;
+	radian = TURN_DIAM / 2;
+	turnDist = pi * radian;
+	numRot = turnDist / WHEEL_CIRCUM;
+	brick.MoveMotorAngleRel(PORTS('RightMotor'), TURN_SPEED, numRot * 360, 'Brake');
+	brick.MoveMotorAngleRel(PORTS('LeftMotor'), -TURN_SPEED, numRot * 360, 'Brake');
 end
 
+% TODO: maybe just use sleep(seconds)
 function stop(duration)
 	
 end
--- auto-nav/init.m
+++ auto-nav/init.m
@@ -1,1 +1,46 @@
-global inAuto wheelCircum turnDiam;
+% * make sure you've run 'brick = ConnectBrick("pogger")' before executing this script
+global IN_AUTO IN_DRIVE WHEEL_CIRCUM TURN_DIAM TURN_SPEED DRIVE_SPEED PORTS COLORS;
+
+botInit();
+init();
+
+function init()
+
+	while IN_AUTO
+
+			if ~IN_DRIVE
+				startDrive();
+				IN_DRIVE = true;
+			end
+
+			if brick.TouchPressed(PORTS('Touch'))
+				stopDrive();
+				IN_DRIVE = false;
+				[rightOpen, leftOpen] = contactScan();
+				if rightOpen
+					turnRight();
+				elseif leftOpen
+					turnLeft();
+				else
+					turnAbout();
+				end
+			elseif rightScan()
+				% maybe don't turn immediately, move forward a bit more
+				% it might be worth measuring where the hallway opening starts and ends
+				% to position the bot in the middle of the opening before turning
+				turnRight();
+			end
+
+			% make sure color sensor checks aren't expensive
+			% if they are add a delay to this check
+			color_rgb = brick.ColorRGB(SensorPort);
+			if color_rgb == COLORS('STOP')
+				pause(3) % stop for 3 seconds
+			elseif color_rgb == COLORS('PICKUP') || color_rgb == COLORS('DROPOFF')
+				IN_AUTO = false;
+				startAdvControl();
+			end
+			
+	end
+	
+end
