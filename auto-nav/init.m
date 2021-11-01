85c1e38a091ec15c7686e93b7bbc6c19d7089fbe
Ryan Woo <ryanw.contact@gmail.com>
2021-11-01 10:24:33 -0700

    update measurements

--- auto-nav/init.m
+++ auto-nav/init.m
@@ -8,14 +8,20 @@ function init(brick)
 	turnSpeed = 50;
 	wheelDiam = 5.715;
 	turnDiam = 12.065;
+	steer_min = 12;
+	steer_max = 21;
+	steer_amt = 10;
+	wall_dist_max = 70;
 	ports = containers.Map(...
 	{"RightMotor", "LeftMotor", "Touch", "Ultra", "Color"},...
 	{"D"         , "A"        , "1"    , "2"    , "3"});
 	colors = containers.Map(...
 	{"STOP"     , "PICKUP" , "DROPOFF"},...
 	{[255, 0, 0], [0, 255, 0], [0, 0, 255]});
+	colorTol = 20;
 	
-	bot = BotController(brick, driveSpeed, turnSpeed, wheelDiam, turnDiam, ports, colors);
+	bot = BotController(brick, driveSpeed, turnSpeed,...
+					wheelDiam, turnDiam, steer_min, steer_max, steer_amt, wall_dist_max, ports, colors, colorTol)
 	bot.beginNav();
 
 end
