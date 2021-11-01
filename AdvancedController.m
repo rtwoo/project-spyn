b1de65d76221e4893d16d658b0b61542dc4a00e4
Ryan Woo <ryanw.contact@gmail.com>
2021-11-01 09:38:06 -0700

    switched AdvancedController char arrays to strings

--- AdvancedController.m
+++ AdvancedController.m
@@ -1,18 +1,18 @@
-fig = figure('Name', 'EV3 Controller', 'KeyPressFcn', @handleKeyDown, 'KeyReleaseFcn', @handleKeyUp);
-right = 'D';
-left = 'A';
-loader = 'C';
+fig = figure("Name", "EV3 Controller", "KeyPressFcn", @handleKeyDown, "KeyReleaseFcn", @handleKeyUp);
+right = "D";
+left = "A";
+loader = "C";
 speed = 100;
 turnSpeed = 50;
 loaderSpeed = 10;
-% brick = 'null';
+% brick = "null";
 
 motorStates = containers.Map(...
-	{'inForward', 'inReverse', 'inLeft', 'inRight', 'inTurnLeft', 'inTurnRight', 'inBrake', 'inRaise', 'inLower'},...
+	{"inForward", "inReverse", "inLeft", "inRight", "inTurnLeft", "inTurnRight", "inBrake", "inRaise", "inLower"},...
 	{false, false, false, false, false, false, false, false, false});
 
 t = timer;
-t.ExecutionMode = 'fixedDelay';
+t.ExecutionMode = "fixedDelay";
 t.TimerFcn = @beep;
 t.UserData = struct(...
 	"Brick", brick...
@@ -47,90 +47,79 @@ function handleKeyDown(src, event)
 	
 	switch event.Key
 
-		case 'w' % forward
-			if ~motorStates('inForward')
+		case "w" % forward
+			if ~motorStates("inForward")
 				overrideMovement(motorStates);
-				brick.MoveMotor(strcat(right, left), speed);
-				fprintf("brick.MoveMotor(strcat(right, left), speed);\n");
-				motorStates('inForward') = true;
+				brick.MoveMotor(right + left, speed);
+				motorStates("inForward") = true;
 			end
-		case 's' % reverse
-			if ~motorStates('inReverse')
+		case "s" % reverse
+			if ~motorStates("inReverse")
 				overrideMovement(motorStates);
-				brick.MoveMotor(strcat(right, left), -speed);
-				fprintf("brick.MoveMotor(strcat(right, left), -speed);\n");
-				motorStates('inReverse') = true;
+				brick.MoveMotor(right + left, -speed);
+				motorStates("inReverse") = true;
 				start(beep);
 			end
-		case 'a' % left
-			if motorStates('inForward')
-				if ~motorStates('inTurnLeft')
-					motorStates('inTurnRight') = false;
+		case "a" % left
+			if motorStates("inForward")
+				if ~motorStates("inTurnLeft")
+					motorStates("inTurnRight") = false;
 					brick.MoveMotor(left, speed);
-					fprintf("brick.MoveMotor(left, speed);\n");
 					brick.MoveMotor(right, 0.5 * speed);
-					fprintf("brick.MoveMotor(left, 0.75 * speed)\n");
-					motorStates('inTurnLeft') = true;
+					motorStates("inTurnLeft") = true;
 				end
-			elseif motorStates('inReverse')
-				if ~motorStates('inTurnLeft')
-					motorStates('inTurnRight') = false;
+			elseif motorStates("inReverse")
+				if ~motorStates("inTurnLeft")
+					motorStates("inTurnRight") = false;
 					brick.MoveMotor(right, -speed);
 					brick.MoveMotor(left, 0.5 * -speed);
-					motorStates('inTurnLeft') = true;
+					motorStates("inTurnLeft") = true;
 				end
-			elseif ~motorStates('inLeft')
+			elseif ~motorStates("inLeft")
 				overrideMovement(motorStates);
 				brick.MoveMotor(left, turnSpeed);
-				fprintf("brick.MoveMotor(right, speed)\n");
 				brick.MoveMotor(right, -turnSpeed);
-				fprintf("brick.MoveMotor(left, -speed)\n");
-				motorStates('inLeft') = true;
+				motorStates("inLeft") = true;
 			end
-		case 'd' % right
-			if motorStates('inForward')
-				if ~motorStates('inTurnRight')
-					motorStates('inTurnLeft') = false;
+		case "d" % right
+			if motorStates("inForward")
+				if ~motorStates("inTurnRight")
+					motorStates("inTurnLeft") = false;
 					brick.MoveMotor(left, 0.5 * speed);
-					fprintf("brick.MoveMotor(right, 0.75 * speed)\n");
 					brick.MoveMotor(right, speed);
-					fprintf("brick.MoveMotor(left, speed)\n");
-					motorStates('inTurnRight') = true;
+					motorStates("inTurnRight") = true;
 				end
-			elseif motorStates('inReverse')
-				if ~motorStates('inTurnRight')
-					motorStates('inTurnLeft') = false;
+			elseif motorStates("inReverse")
+				if ~motorStates("inTurnRight")
+					motorStates("inTurnLeft") = false;
 					brick.MoveMotor(right, 0.5 * -speed);
 					brick.MoveMotor(left, -speed);
-					motorStates('inTurnRight') = true;
+					motorStates("inTurnRight") = true;
 				end
-			elseif ~motorStates('inRight')
+			elseif ~motorStates("inRight")
 				overrideMovement(motorStates);
 				brick.MoveMotor(right, turnSpeed);
-				fprintf("brick.MoveMotor(left, speed)\n");
 				brick.MoveMotor(left, -turnSpeed);
-				fprintf("brick.MoveMotor(right, -speed)\n");
-				motorStates('inRight') = true;
+				motorStates("inRight") = true;
 			end
-		case 'space'
-			if ~motorStates('inBrake')
-				brick.StopAllMotors('Brake');
-				fprintf("brick.StopAllMotors('Brake')\n");
-				motorStates('inBrake') = true;
+		case "space"
+			if ~motorStates("inBrake")
+				brick.StopAllMotors("Brake");
+				motorStates("inBrake") = true;
 			end
-		case 'semicolon'
-			if ~motorStates('inRaise')
+		case "semicolon"
+			if ~motorStates("inRaise")
 				brick.MoveMotor(loader, loaderSpeed);
-				motorStates('inRaise') = true;
-				motorStates('inLower') = false;
+				motorStates("inRaise") = true;
+				motorStates("inLower") = false;
 			end
-		case 'quote'
-			if ~motorStates('inLower')
+		case "quote"
+			if ~motorStates("inLower")
 				brick.MoveMotor(loader, -loaderSpeed);
-				motorStates('inLower') = true;
-				motorStates('inRaise') = false;
+				motorStates("inLower") = true;
+				motorStates("inRaise") = false;
 			end
-		case 'escape'
+		case "escape"
 			close(src);
 % 			IN_AUTO = true;
 % 			init();
@@ -154,65 +143,55 @@ function handleKeyUp(src, event)
 	% fprintf("Released: " + event.Key + "\n");
 	switch event.Key
 
-		case 'w' % forward
-			if motorStates('inForward')
-					brick.StopMotor(strcat(right, left), 'Coast');
-					fprintf("brick.StopMotor(right + left, 'Coast')\n");
+		case "w" % forward
+			if motorStates("inForward")
+					brick.StopMotor(right + left, "Coast");
 					overrideMovement(userData.MotorStates);
 			end
-		case 's' % reverse
-			if motorStates('inReverse')
-					brick.StopMotor(strcat(right, left), 'Coast');
-					fprintf("brick.StopMotor(right + left, 'Coast')\n");
-					motorStates('inReverse') = false;
+		case "s" % reverse
+			if motorStates("inReverse")
+					brick.StopMotor(right + left, "Coast");
+					motorStates("inReverse") = false;
 					stop(beep);
 			end
-		case 'a' % left
-			if motorStates('inForward') && motorStates('inTurnLeft')
+		case "a" % left
+			if motorStates("inForward") && motorStates("inTurnLeft")
 				brick.MoveMotor(right, speed);
-				fprintf("brick.MoveMotor(left, speed)\n");
-				motorStates('inTurnLeft') = false;
-			elseif motorStates('inReverse') && motorStates('inTurnLeft')
+				motorStates("inTurnLeft") = false;
+			elseif motorStates("inReverse") && motorStates("inTurnLeft")
 				brick.MoveMotor(left, -speed);
-				motorStates('inTurnLeft') = false;
-			elseif motorStates('inLeft')
-					brick.StopMotor(right, 'Coast');
-					fprintf("brick.StopMotor(right, 'Coast')\n");
-					brick.StopMotor(left, 'Coast');
-					fprintf("brick.StopMotor(left, 'Coast')\n");
-					motorStates('inLeft') = false;
+				motorStates("inTurnLeft") = false;
+			elseif motorStates("inLeft")
+					brick.StopMotor(right + left, "Coast");
+					motorStates("inLeft") = false;
 			end
-		case 'd' % right
-			if motorStates('inForward') && motorStates('inTurnRight')
+		case "d" % right
+			if motorStates("inForward") && motorStates("inTurnRight")
 					brick.MoveMotor(left, speed);
-					fprintf("brick.MoveMotor(right, speed)\n");
-					motorStates('inTurnRight') = false;
-			elseif motorStates('inReverse') && motorStates('inTurnRight')
+					motorStates("inTurnRight") = false;
+			elseif motorStates("inReverse") && motorStates("inTurnRight")
 					brick.MoveMotor(right, -speed);
-					motorStates('inTurnRight') = false;
-			elseif motorStates('inRight')
-					brick.StopMotor(left, 'Coast');
-					fprintf("brick.StopMotor(left, 'Coast')\n");
-					brick.StopMotor(right, 'Coast');
-					fprintf("brick.StopMotor(right, 'Coast')\n");
-					motorStates('inRight') = false;
+					motorStates("inTurnRight") = false;
+			elseif motorStates("inRight")
+					brick.StopMotor(right + left, "Coast");
+					motorStates("inRight") = false;
 			end
-		case 'space'
-			if motorStates('inBrake')
-				motorStates('inBrake') = false;
+		case "space"
+			if motorStates("inBrake")
+				motorStates("inBrake") = false;
 			end
-		case 'semicolon'
-			if motorStates('inRaise')
-				motorStates('inRaise') = false;
-				if ~motorStates('inLower')
-					brick.StopMotor(loader, 'Coast');
+		case "semicolon"
+			if motorStates("inRaise")
+				motorStates("inRaise") = false;
+				if ~motorStates("inLower")
+					brick.StopMotor(loader, "Coast");
 				end
 			end
-		case 'quote'
-			if motorStates('inLower')
-				motorStates('inLower') = false;
-				if ~motorStates('inRaise')
-						brick.StopMotor(loader, 'Coast');
+		case "quote"
+			if motorStates("inLower")
+				motorStates("inLower") = false;
+				if ~motorStates("inRaise")
+						brick.StopMotor(loader, "Coast");
 				end
 			end
 	
