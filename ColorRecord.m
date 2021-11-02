da8c87d7ca0bff3b4f627f63138840b256a237a1
Ryan Woo <ryanw.contact@gmail.com>
2021-11-02 09:05:06 -0700

    Added RGB calibration script

---
+++ ColorRecord.m
@@ -0,0 +1,32 @@
+% ! brick.SetColorMode(PORTS("Color"), 4);
+
+reds = [];
+greens = [];
+blues = [];
+
+t = timer;
+t.TasksToExecute = 10;
+t.ExecutionMode = 'fixedDelay';
+t.TimerFcn = @record;
+t.UserData = struct(...
+	'Brick', brick,...
+	'Reds', reds,...
+	'Greens', greens,...
+	'Blues', blues
+);
+
+function record(src, ~)
+
+	userData = src.UserData;
+	brick = userData.Brick;
+	reds = userData.Reds;
+	greens = userData.Greens;
+	blues = userData.Blues;
+	
+	colors = brick.ColorRGB();
+
+	reds = [reds, colors(1)];
+	greens = [greens, colors(2)];
+	blues = [blues, colors(3)];
+
+end
